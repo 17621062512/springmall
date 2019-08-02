01890a2982dea7b0ec79c0f2ed92d6425e0c8fef
limenglong <limenglong@juxinli.com>
2019-08-02 18:24:12 +0800

    使用MyBatis Generator自动生成MySql对应表的model、mapper、mapper.xml等映射文件

---
+++ mall-01/src/main/java/com/leemon/mall/config/MyBatisConfig.java
@@ -0,0 +1,14 @@
+package com.leemon.mall.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author limenglong
+ * @create 2019-08-02 17:45
+ * @desc MyBatis配置类
+ **/
+@Configuration
+@MapperScan("com.leemon.mall.mbg.mapper")
+public class MyBatisConfig {
+}
