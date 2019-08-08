19fe4a078e84d2ec760e56f7749b06202c491ea1
limenglong <limenglong@juxinli.com>
2019-08-08 11:34:25 +0800

    使用swagger2生成API文档

---
+++ mall-01/src/main/java/com/leemon/mall/config/Swagger2Config.java
@@ -0,0 +1,48 @@
+package com.leemon.mall.config;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author limenglong
+ * @create 2019-08-07 11:32
+ * @desc Swagger2API文档配置
+ **/
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //为当前包下controller生成API文档
+                .apis(RequestHandlerSelectors.basePackage("com.leemon.mall.controller"))
+                //为有@Api注解的Controller生成API文档
+//                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                //为有@ApiOperation注解的方法生成API文档
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger2UI演示")
+                .description("spring-mall")
+                .contact(new Contact("leemon", "www.baidu.com", "17621062512@163.com"))
+                .version("1.0")
+                .build();
+    }
+}
--- mall-01/src/main/java/com/leemon/mall/controller/PmsBrandController.java
+++ mall-01/src/main/java/com/leemon/mall/controller/PmsBrandController.java
@@ -4,6 +4,8 @@ import com.leemon.mall.common.api.CommenPage;
 import com.leemon.mall.common.api.CommenResult;
 import com.leemon.mall.mbg.model.PmsBrand;
 import com.leemon.mall.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +19,7 @@ import java.util.List;
  * @desc 品牌管理Controller
  **/
 
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
 @RestController
 @RequestMapping("/brand")
 public class PmsBrandController {
@@ -26,13 +29,14 @@ public class PmsBrandController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
-
+    @ApiOperation("获取所有品牌列表")
     @GetMapping("/listAll")
     public CommenResult<List<PmsBrand>> getBrandList() {
         return CommenResult.success(pmsBrandService.listAllBrand());
     }
 
 
+    @ApiOperation("添加品牌")
     @PostMapping("/create")
     public CommenResult createBrand(@RequestBody PmsBrand pmsBrand) {
         CommenResult commenResult;
@@ -47,6 +51,7 @@ public class PmsBrandController {
         return commenResult;
     }
 
+    @ApiOperation("更新指定ID品牌信息")
     @PostMapping("/update/{id}")
     public CommenResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand pmsBrandDto) {
         CommenResult commenResult;
@@ -62,6 +67,7 @@ public class PmsBrandController {
     }
 
 
+    @ApiOperation("删除指定id的品牌")
     @GetMapping("/delete/{id}")
     public CommenResult deleteBrand(@PathVariable("id") Long id) {
         CommenResult commenResult;
@@ -76,6 +82,7 @@ public class PmsBrandController {
         return commenResult;
     }
 
+    @ApiOperation("分页查询品牌列表")
     @GetMapping("/list")
     public CommenResult<CommenPage> listBrand(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
                                               @RequestParam(value = "pageSize", defaultValue = "3") Integer pageSize) {
@@ -85,6 +92,7 @@ public class PmsBrandController {
 
     }
 
+    @ApiOperation("获取指定id的品牌信息")
     @GetMapping("/{id}")
     public CommenResult brand(@PathVariable("id") Long id) {
         PmsBrand brand = pmsBrandService.getBrand(id);
